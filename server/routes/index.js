7271be8520895fea773f1ce3c277f6b43c3f5a32
bushmb <bushmb@gmail.com>
2017-04-02 17:16:25 -0600

    fix default index.html location to build folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ var path = require('path')
 
 
 router.get('*', function(req, res, next) {
-  const index = path.join(__dirname, '../../client/public', 'index.html');
+  const index = path.join(__dirname, '../../client/build', 'index.html');
   res.sendFile(index);
 });
 
